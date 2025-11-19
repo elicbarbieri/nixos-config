85058e9e5accc07c00c51300b2504bb204129ebc
Eli Barbieri <elicbarbieri@gmail.com>
2025-11-18 18:45:33 -0800

    adding hyprlock to system startup

--- home/desktop.nix
+++ home/desktop.nix
@@ -4,20 +4,20 @@
   imports = [
     ./base.nix
   ];
-  
+
   # Desktop-specific configuration (GUI apps, theming, etc.)
   home.file = {
-    ".config/btop".source = ../dotfiles/btop;
-    ".config/git".source = ../dotfiles/git;
-    ".config/gtk-3.0".source = ../dotfiles/gtk/gtk-3.0;
-    ".config/gtk-4.0".source = ../dotfiles/gtk/gtk-4.0;
-    ".config/hypr".source = ../dotfiles/hypr;
-    ".config/kitty".source = ../dotfiles/kitty;
-    ".config/Kvantum".source = ../dotfiles/Kvantum;
-    ".config/lazygit".source = ../dotfiles/lazygit;
-    ".config/qt6ct".source = ../dotfiles/qt6ct;
-    ".config/rofi".source = ../dotfiles/rofi;
-    ".config/ruff".source = ../dotfiles/ruff;
+    ".config/btop".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/btop";
+    ".config/git".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/git";
+    ".config/gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/gtk/gtk-3.0";
+    ".config/gtk-4.0".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/gtk/gtk-4.0";
+    ".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/hypr";
+    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/kitty";
+    ".config/Kvantum".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/Kvantum";
+    ".config/lazygit".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/lazygit";
+    ".config/qt6ct".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/qt6ct";
+    ".config/rofi".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/rofi";
+    ".config/ruff".source = config.lib.file.mkOutOfStoreSymlink "/home/elicb/nixos-config/dotfiles/ruff";
   };
 
 
